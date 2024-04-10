f8f20be795e8f2f6d92e812c0b1b7483d18a225a
QaisarTanvir <qaisar.tanvir@outlook.com>
2024-04-11 03:21:09 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,15 @@ setup(
             "codebase-to-text = codebase_to_text.codebase_to_text:main",
         ]
     },
-    
-    install_requires=[
-          'python-docx'
-      ],
     author="Qaisar Tanvir",
     author_email="qaisartanvir.dev@gmail.com",
     description="A Python package to convert codebase to text",
-    long_description="Converts a codebase (folder structure with files) into a single text file or a Microsoft Word document (.docx), preserving folder structure and file contents",
-    download_url="https://github.com/QaisarRajput/codebase_to_text/archive/refs/tags/1.0.0.tar.gz"
+    long_description=open("docs/README.md").read(),
+    download_url="https://github.com/QaisarRajput/codebase_to_text/archive/refs/tags/1.0.0.tar.gz",
     long_description_content_type="text/markdown",
+    keywords = ["codebase, code conversion, text conversion, folder structure, file contents, text extraction, document conversion, Python package, GitHub repository, command-line tool, code analysis, file parsing, code documentation, formatting preservation, readability"],
     license="MIT",
-    keywords="codebase text conversion",
-    url="https://github.com/QaisarRajput/codebase-to-text",
+    url="https://github.com/QaisarRajput/codebase_to_text",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
