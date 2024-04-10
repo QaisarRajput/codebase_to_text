55837c4befd858196daf1fb12ee16f37bdd74d59
QaisarTanvir <qaisar.tanvir@outlook.com>
2024-04-11 03:21:27 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codebase-to-text",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=["python-docx"],
     entry_points={
