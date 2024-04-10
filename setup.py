198571c169a149ba52f38ef76401e91c10249d60
QaisarTanvir <qaisar.tanvir@outlook.com>
2024-04-11 03:03:04 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     author="Qaisar Tanvir",
     author_email="qaisartanvir.dev@gmail.com",
     description="A Python package to convert codebase to text",
-    long_description=open("docs/README.md").read(),
+    long_description="Converts a codebase (folder structure with files) into a single text file or a Microsoft Word document (.docx), preserving folder structure and file contents",
+    download_url="https://github.com/QaisarRajput/codebase_to_text/archive/refs/tags/1.0.0.tar.gz"
     long_description_content_type="text/markdown",
     license="MIT",
     keywords="codebase text conversion",
