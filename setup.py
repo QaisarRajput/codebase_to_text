45c26e95cccc4c0771f037608a1b52228cbe439d
QaisarTanvir <qaisar.tanvir@outlook.com>
2024-04-11 04:20:55 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Qaisar Tanvir",
     author_email="qaisartanvir.dev@gmail.com",
     description="A Python package to convert codebase to text",
-    long_description=open("docs/README.md").read(),
+    long_description=open("README.md").read(),
     download_url="https://github.com/QaisarRajput/codebase_to_text/archive/refs/tags/1.0.1.tar.gz",
     long_description_content_type="text/markdown",
     keywords = ["codebase, code conversion, text conversion, folder structure, file contents, text extraction, document conversion, Python package, GitHub repository, command-line tool, code analysis, file parsing, code documentation, formatting preservation, readability"],
