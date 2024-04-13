d7c31d4be6830a2412f83d93422fd3b2d52c7f88
QaisarTanvir <qaisar.tanvir@outlook.com>
2024-04-14 03:54:36 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
-
+import os
+print(os.path.dirname(__file__))
 setup(
     name="codebase-to-text",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
-    install_requires=["python-docx"],
+    install_requires=["python-docx","gitpython","tempfile"],
     entry_points={
         "console_scripts": [
             "codebase-to-text = codebase_to_text.codebase_to_text:main",
@@ -13,11 +14,12 @@ setup(
     author="Qaisar Tanvir",
     author_email="qaisartanvir.dev@gmail.com",
     description="A Python package to convert codebase to text",
-    long_description=open("README.md").read(),
-    download_url="https://github.com/QaisarRajput/codebase_to_text/archive/refs/tags/1.0.3.tar.gz",
+    license="MIT",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    download_url="https://github.com/QaisarRajput/codebase_to_text/archive/refs/tags/1.0.4.tar.gz",
     long_description_content_type="text/markdown",
     keywords = ["codebase, code conversion, text conversion, folder structure, file contents, text extraction, document conversion, Python package, GitHub repository, command-line tool, code analysis, file parsing, code documentation, formatting preservation, readability"],
-    license="MIT",
+    
     url="https://github.com/QaisarRajput/codebase_to_text",
     classifiers=[
         "License :: OSI Approved :: MIT License",
